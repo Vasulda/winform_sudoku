39192b1b30e4e0764ec2bfb04974fcda08c3e46e
Vasulda <vaclavkubon2@gmail.com>
2020-04-04 19:01:55 +0200

    finished

---
+++ SudokuProject/SudokuProject/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace SudokuProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SolveButton = new System.Windows.Forms.Button();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.But1 = new System.Windows.Forms.Button();
+            this.But7 = new System.Windows.Forms.Button();
+            this.But2 = new System.Windows.Forms.Button();
+            this.But9 = new System.Windows.Forms.Button();
+            this.But6 = new System.Windows.Forms.Button();
+            this.But5 = new System.Windows.Forms.Button();
+            this.But4 = new System.Windows.Forms.Button();
+            this.But8 = new System.Windows.Forms.Button();
+            this.But3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EraseBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SolveButton
+            // 
+            this.SolveButton.AutoSize = true;
+            this.SolveButton.Location = new System.Drawing.Point(517, 12);
+            this.SolveButton.Name = "SolveButton";
+            this.SolveButton.Size = new System.Drawing.Size(152, 44);
+            this.SolveButton.TabIndex = 0;
+            this.SolveButton.Tag = "SolveButton";
+            this.SolveButton.Text = "Solve";
+            this.SolveButton.UseVisualStyleBackColor = true;
+            this.SolveButton.Click += new System.EventHandler(this.SolveButton_Click);
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(517, 62);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(152, 44);
+            this.RestartButton.TabIndex = 1;
+            this.RestartButton.Tag = "RestartButton";
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            this.RestartButton.Click += new System.EventHandler(this.RestartButton_Click);
+            // 
+            // But1
+            // 
+            this.But1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But1.Location = new System.Drawing.Point(488, 244);
+            this.But1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But1.Name = "But1";
+            this.But1.Size = new System.Drawing.Size(41, 45);
+            this.But1.TabIndex = 3;
+            this.But1.Tag = "But1";
+            this.But1.Text = "1";
+            this.But1.UseVisualStyleBackColor = true;
+            this.But1.Click += new System.EventHandler(this.But1_Click);
+            // 
+            // But7
+            // 
+            this.But7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But7.Location = new System.Drawing.Point(488, 343);
+            this.But7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But7.Name = "But7";
+            this.But7.Size = new System.Drawing.Size(41, 45);
+            this.But7.TabIndex = 4;
+            this.But7.Tag = "But1";
+            this.But7.Text = "7";
+            this.But7.UseVisualStyleBackColor = true;
+            this.But7.Click += new System.EventHandler(this.But7_Click);
+            // 
+            // But2
+            // 
+            this.But2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But2.Location = new System.Drawing.Point(533, 244);
+            this.But2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But2.Name = "But2";
+            this.But2.Size = new System.Drawing.Size(41, 45);
+            this.But2.TabIndex = 5;
+            this.But2.Tag = "But1";
+            this.But2.Text = "2";
+            this.But2.UseVisualStyleBackColor = true;
+            this.But2.Click += new System.EventHandler(this.But2_Click);
+            // 
+            // But9
+            // 
+            this.But9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But9.Location = new System.Drawing.Point(579, 343);
+            this.But9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But9.Name = "But9";
+            this.But9.Size = new System.Drawing.Size(41, 45);
+            this.But9.TabIndex = 6;
+            this.But9.Tag = "But1";
+            this.But9.Text = "9";
+            this.But9.UseVisualStyleBackColor = true;
+            this.But9.Click += new System.EventHandler(this.But9_Click);
+            // 
+            // But6
+            // 
+            this.But6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But6.Location = new System.Drawing.Point(579, 293);
+            this.But6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But6.Name = "But6";
+            this.But6.Size = new System.Drawing.Size(41, 45);
+            this.But6.TabIndex = 7;
+            this.But6.Tag = "But1";
+            this.But6.Text = "6";
+            this.But6.UseVisualStyleBackColor = true;
+            this.But6.Click += new System.EventHandler(this.But6_Click);
+            // 
+            // But5
+            // 
+            this.But5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But5.Location = new System.Drawing.Point(533, 293);
+            this.But5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But5.Name = "But5";
+            this.But5.Size = new System.Drawing.Size(41, 45);
+            this.But5.TabIndex = 8;
+            this.But5.Tag = "But1";
+            this.But5.Text = "5";
+            this.But5.UseVisualStyleBackColor = true;
+            this.But5.Click += new System.EventHandler(this.But5_Click);
+            // 
+            // But4
+            // 
+            this.But4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But4.Location = new System.Drawing.Point(488, 293);
+            this.But4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But4.Name = "But4";
+            this.But4.Size = new System.Drawing.Size(41, 45);
+            this.But4.TabIndex = 9;
+            this.But4.Tag = "But1";
+            this.But4.Text = "4";
+            this.But4.UseVisualStyleBackColor = true;
+            this.But4.Click += new System.EventHandler(this.But4_Click);
+            // 
+            // But8
+            // 
+            this.But8.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But8.Location = new System.Drawing.Point(533, 343);
+            this.But8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But8.Name = "But8";
+            this.But8.Size = new System.Drawing.Size(41, 45);
+            this.But8.TabIndex = 10;
+            this.But8.Tag = "But1";
+            this.But8.Text = "8";
+            this.But8.UseVisualStyleBackColor = true;
+            this.But8.Click += new System.EventHandler(this.But8_Click);
+            // 
+            // But3
+            // 
+            this.But3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.But3.Location = new System.Drawing.Point(579, 244);
+            this.But3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.But3.Name = "But3";
+            this.But3.Size = new System.Drawing.Size(41, 45);
+            this.But3.TabIndex = 11;
+            this.But3.Tag = "But1";
+            this.But3.Text = "3";
+            this.But3.UseVisualStyleBackColor = true;
+            this.But3.Click += new System.EventHandler(this.But3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 26);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // EraseBtn
+            // 
+            this.EraseBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EraseBtn.Location = new System.Drawing.Point(625, 244);
+            this.EraseBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.EraseBtn.Name = "EraseBtn";
+            this.EraseBtn.Size = new System.Drawing.Size(56, 144);
+            this.EraseBtn.TabIndex = 13;
+            this.EraseBtn.Text = "Erase";
+            this.EraseBtn.UseVisualStyleBackColor = true;
+            this.EraseBtn.Click += new System.EventHandler(this.EraseBtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 42);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EraseBtn);
+            this.Controls.Add(this.But3);
+            this.Controls.Add(this.But8);
+            this.Controls.Add(this.But4);
+            this.Controls.Add(this.But5);
+            this.Controls.Add(this.But6);
+            this.Controls.Add(this.But9);
+            this.Controls.Add(this.But2);
+            this.Controls.Add(this.But7);
+            this.Controls.Add(this.But1);
+            this.Controls.Add(this.RestartButton);
+            this.Controls.Add(this.SolveButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SolveButton;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Button But1;
+        private System.Windows.Forms.Button But7;
+        private System.Windows.Forms.Button But2;
+        private System.Windows.Forms.Button But9;
+        private System.Windows.Forms.Button But6;
+        private System.Windows.Forms.Button But5;
+        private System.Windows.Forms.Button But4;
+        private System.Windows.Forms.Button But8;
+        private System.Windows.Forms.Button But3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.Button EraseBtn;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SudokuProject/SudokuProject/Form1.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SudokuProject
+{
+    public partial class Form1 : Form
+    {
+        int chosenNum, disabledBut = 0;
+        public void EnableButtons()
+        {
+            switch (disabledBut)
+            {
+                case 1:
+                    But1.Enabled = true;
+                    break;
+                case 2:
+                    But2.Enabled = true;
+                    break;
+                case 3:
+                    But3.Enabled = true;
+                    break;
+                case 4:
+                    But4.Enabled = true;
+                    break;
+                case 5:
+                    But5.Enabled = true;
+                    break;
+                case 6:
+                    But6.Enabled = true;
+                    break;
+                case 7:
+                    But7.Enabled = true;
+                    break;
+                case 8:
+                    But8.Enabled = true;
+                    break;
+                case 9:
+                    But9.Enabled = true;
+                    break;
+            }
+        }
+
+        //lepsi by byla array buttonu ale tak jsem debil no
+        public void But1_Click(object sender, EventArgs e)
+        {
+            Button But1 = sender as Button;
+            chosenNum = 1;
+            EnableButtons();
+            disabledBut = 1;
+            But1.Enabled = false;
+
+        }
+        public void But2_Click(object sender, EventArgs e)
+        {
+            Button But2 = sender as Button;
+            chosenNum = 2;
+            EnableButtons();
+            disabledBut = 2;
+            But2.Enabled = false;
+        }
+        public void But3_Click(object sender, EventArgs e)
+        {
+            Button But3 = sender as Button;
+            chosenNum = 3;
+            EnableButtons();
+            disabledBut = 3;
+            But3.Enabled = false;
+        }
+        public void But4_Click(object sender, EventArgs e)
+        {
+            Button But4 = sender as Button;
+            chosenNum = 4;
+            EnableButtons();
+            disabledBut = 4;
+            But4.Enabled = false;
+        }
+        public void But5_Click(object sender, EventArgs e)
+        {
+            Button But5 = sender as Button;
+            chosenNum = 5;
+            EnableButtons();
+            disabledBut = 5;
+            But5.Enabled = false;
+        }
+        public void But6_Click(object sender, EventArgs e)
+        {
+            Button But6 = sender as Button;
+            chosenNum = 6;
+            EnableButtons();
+            disabledBut = 6;
+            But6.Enabled = false;
+        }
+        private void But7_Click(object sender, EventArgs e)
+        {
+            Button But7 = sender as Button;
+            chosenNum = 7;
+            EnableButtons();
+            disabledBut = 7;
+            But7.Enabled = false;
+        }
+        private void But8_Click(object sender, EventArgs e)
+        {
+            Button But8 = sender as Button;
+            chosenNum = 8;
+            EnableButtons();
+            disabledBut = 8;
+            But8.Enabled = false;
+        }
+        private void But9_Click(object sender, EventArgs e)
+        {
+            Button But9 = sender as Button;
+            chosenNum = 9;
+            EnableButtons();
+            disabledBut = 9;
+            But9.Enabled = false;
+        }
+        private void EraseBtn_Click(object sender, EventArgs e)
+        {
+            Button EraseBtn = sender as Button;
+            chosenNum = 0;
+        }
+
+
+
+        public Button[,] box = new Button[9, 9];
+        public int[,] grid = new int[9, 9] {
+
+                {0, 5, 3, 0, 0, 0, 7, 9, 0},
+                {0, 0, 9, 7, 8, 2, 6, 0, 0},
+                {0, 0, 0, 5, 0, 3, 0, 0, 0},
+                {0, 0, 0, 4, 0, 6, 0, 0, 0},
+                {0, 4, 0, 0, 0, 0, 0, 6, 0},
+                {8, 0, 5, 1, 0, 9, 3, 0, 2},
+                {0, 0, 8, 9, 3, 1, 4, 0, 0},
+                {9, 0, 0, 0, 0, 0, 0, 0, 6},
+                {0, 0, 4, 0, 0, 0, 8, 0, 0}
+            };
+
+
+        int offset = 0;
+        int boxSide = 40, x = 50, y = 0;
+        //int numberOfRecursions = 0;
+
+
+
+        private void RestartButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        int numSize = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.SetStyle(ControlStyles.Selectable, false);
+
+            for (int i = 0; i < 9; i++)
+            {
+                x += (i % 3 == 0) ? 45 : 40;
+
+                for (int j = 0; j < 9; j++)
+                {
+                    y += (j % 3 == 0) ? 45 : 40;
+
+                    box[i, j] = new Button();
+                    box[i, j].Size = new Size(boxSide, boxSide);
+                    this.Controls.Add(box[i, j]);
+                    box[i, j].Location = new Point(x, y);
+                    box[i, j].Font = new Font(box[i, j].Font.FontFamily, numSize);
+                    box[i, j].Name = i + " " + j;
+                    box[i, j].Click += new EventHandler(ButtonClick);
+                }
+
+                y = offset;
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (grid[i, j] != 0)
+                    {
+                        box[i, j].Text = grid[i, j].ToString();
+                        box[i, j].Enabled = false;
+                    }
+                }
+            }
+
+        }
+        //---------------------------------
+
+        protected void ButtonClick(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            if (chosenNum == 0)
+            {
+                button.Text = " ";
+
+            }
+            else button.Text = chosenNum.ToString();
+
+            string[] split = button.Name.Split(new Char[] { ' ' });
+
+            int x = System.Convert.ToInt32(split[0]);
+            int y = System.Convert.ToInt32(split[1]);
+
+            grid[x, y] = chosenNum;
+
+            if (chosenNum != 0)
+            {
+                if (!(isRowOK(grid, x, y) == true && isCollumOK(grid, x, y) && isSectorOK(grid, x, y) == true))
+                {
+                    button.BackColor = Color.Red;
+                }
+                else button.BackColor = default(Color);
+            }
+            else button.BackColor = default(Color);
+
+/*
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (grid[i, j] != 0) MessageBox.Show("VYHRAL JSI");
+                }
+            }
+*/
+        }
+        // SOLVING ALGHORITHM -----------------------------------------------------------------------------------------------------------------
+
+        public int solveSudoku(int[,] grid, int last_i, int last_j)
+        {
+            bool isThereSpace = false;
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+
+                    if (grid[i, j] == 0)
+                    {
+                        last_i = i;
+                        last_j = j;
+
+                        isThereSpace = true;
+                        break;// jeste existuje prazdne policko
+                    }
+                   
+                }
+                if (isThereSpace == true) break;
+            }
+         
+
+            if (isThereSpace == false)
+            {
+                 for (int i = 0; i < 9; i++)
+                 {
+                     for (int j = 0; j < 9; j++)
+                     {
+                         box[i, j].Text = grid[i, j].ToString();
+                     }
+                 }
+                 //insert printing 
+                return 1; //sudoku je finito 
+            } else {
+                do
+                {
+                    do
+                    {
+                        grid[last_i, last_j] += 1;
+                       
+                        if (grid[last_i, last_j] == 10)
+                        {
+                        
+                            grid[last_i, last_j] = 0; //back track
+                            return -1;
+                        }
+
+                    } while (!(isRowOK(grid, last_i, last_j) == true && isCollumOK(grid, last_i, last_j) == true && isSectorOK(grid, last_i, last_j) == true));
+
+                } while (solveSudoku(grid, last_i, last_j) == -1);
+
+                return 1;
+            }
+        }
+        // IN GAME LOGIC ------------------------------------------------------------------------------------------------------------------------
+        public bool isRowOK(int[,] grid, int last_i, int last_j)
+        {
+
+            for (int j = 0; j < 9; j++)
+            {
+
+                if (j != last_j)
+                {
+                    if (grid[last_i, j] == grid[last_i, last_j]) return false; // nektere hodnota v polickach je stejna
+                }
+            }
+            return true;
+        }
+
+      
+
+        public bool isCollumOK(int[,] grid, int last_i, int last_j)
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+
+                if (i != last_i)
+                {
+
+                    if (grid[i, last_j] == grid[last_i, last_j]) return false; // nektere hodnota v polickach je stejna
+                }
+            }
+            return true;
+        }
+
+        public bool isSectorOK(int[,] grid, int last_i, int last_j)
+        {
+
+            int start_i = 0, start_j = 0; // roh daneho sektoru 0,3 0,6 ....
+
+            if ((last_i >= 0 && last_i <= 2) && (last_j >= 0 && last_j <= 2))
+            {
+                start_i = 0;
+                start_j = 0;
+            }
+
+            if ((last_i >= 0 && last_i <= 2) && (last_j >= 3 && last_j <= 5))
+            {
+                start_i = 0;
+                start_j = 3;
+            }
+
+            if ((last_i >= 0 && last_i <= 2) && (last_j >= 6 && last_j <= 8))
+            {
+                start_i = 0;
+                start_j = 6;
+            }
+
+            if ((last_i >= 3 && last_i <= 5) && (last_j >= 0 && last_j <= 2))
+            {
+                start_i = 3;
+                start_j = 0;
+            }
+
+            if ((last_i >= 3 && last_i <= 5) && (last_j >= 3 && last_j <= 5))
+            {
+                start_i = 3;
+                start_j = 3;
+            }
+
+            if ((last_i >= 3 && last_i <= 5) && (last_j >= 6 && last_j <= 8))
+            {
+                start_i = 3;
+                start_j = 6;
+            }
+
+            if ((last_i >= 6 && last_i <= 8) && (last_j >= 0 && last_j <= 2))
+            {
+                start_i = 6;
+                start_j = 0;
+            }
+
+            if ((last_i >= 6 && last_i <= 8) && (last_j >= 3 && last_j <= 5))
+            {
+                start_i = 6;
+                start_j = 3;
+            }
+
+            if ((last_i >= 6 && last_i <= 8) && (last_j >= 6 && last_j <= 8))
+            {
+                start_i = 6;
+                start_j = 6;
+            }
+
+            for (int i = start_i; i < start_i + 3; i++)
+            {
+                for (int j = start_j; j < start_j + 3; j++)
+                {
+                    if (i != last_i && j != last_j)
+                        if (grid[i, j] == grid[last_i, last_j]) return false;
+                }
+            }
+            return true;
+        }
+
+        public void SolveButton_Click(object sender, EventArgs e)
+        {
+            solveSudoku(grid, 0, 0);
+
+        }
+
+    }
+}
